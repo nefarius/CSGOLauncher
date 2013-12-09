82e54371d4f3ba05febf56eae0a674f1c390555b
Nefarius <nefarius@dhmx.at>
2013-12-09 13:47:59 +0100

    Added more comments

--- CSGOLauncher/LauncherConfig.cs
+++ CSGOLauncher/LauncherConfig.cs
@@ -6,21 +6,33 @@ namespace CSGOLauncher
 {
     static class LauncherConfig
     {
+        /// <summary>
+        /// Returns the directory the current Assembly is in.
+        /// </summary>
         public static string CurrentDirectory
         {
             get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
         }
 
+        /// <summary>
+        /// Returns configuration file name.
+        /// </summary>
         private static string ConfigFileName
         {
             get { return "csgo_launcher.cfg"; }
         }
 
+        /// <summary>
+        /// Returns full qualified file name path to config file.
+        /// </summary>
         private static string ConfigFilePath
         {
             get { return Path.Combine(CurrentDirectory, ConfigFileName); }
         }
 
+        /// <summary>
+        /// Returns saved configuration if read and stores it on disk if set.
+        /// </summary>
         public static CSGOAttributes StoredConfiguration
         {
             get
