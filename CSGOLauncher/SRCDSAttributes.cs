b8ab990553011a94cf8adc300594019dccb6dce7
Nefarius <nefarius@dhmx.at>
2013-12-10 18:30:48 +0100

    Ignored IP-Attributes

--- CSGOLauncher/SRCDSAttributes.cs
+++ CSGOLauncher/SRCDSAttributes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace CSGOLauncher
 {
@@ -16,7 +17,11 @@ namespace CSGOLauncher
         public bool UseRcon { get; set; }
         // Doesn't work on WIN32
         public bool AutoUpdate { get; set; }
+        // This property tends to change, don't save it
+        [XmlIgnore]
         public string PrivateIP { get; set; }
+        // This property tends to change, don't save it
+        [XmlIgnore]
         public string PublicIP { get; set; }
         public string Map { get; set; }
         public bool LanMode { get; set; }
