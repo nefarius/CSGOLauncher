736454a62d5b7247bc26d6ec893752456f1e7f28
Nefarius <nefarius@dhmx.at>
2013-12-11 16:10:39 +0100

    Removed unused code

--- CSGOLauncher/UPnP.cs
+++ CSGOLauncher/UPnP.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Net.Sockets;
-using System.Net;
-using System.Xml;
 using System.IO;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml;
 using CSGOLauncher;
 
 /*
